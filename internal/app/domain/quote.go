f2c3f9584e2db1282e5d03a9ea72cfa377ac08ac
RMarmolejo90 <richardmarmolejo@gmail.com>
2024-09-05 16:03:06 -0400

    update quote model

--- internal/app/domain/quote.go
+++ internal/app/domain/quote.go
@@ -1,4 +1,5 @@
 package domain
+
 import (
 	"time"
 
@@ -12,4 +13,5 @@ type Quote struct {
 	QuoteDate   time.Time
 	Price       float64 `gorm:"not null"`
 	Description string  `gorm:"type:text;not null"`
+	Status      string  `gorm:"type:varchar(20);not null;check:status IN ('not sent', 'awaiting response', 'approved', 'declined')"`
 }
